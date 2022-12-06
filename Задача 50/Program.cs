143c6ae526c2300fbc0cdab27537875fe07f494d
Yuri <urakokurin197@gmail.com>
2022-12-06 20:26:42 +0400

    Sem7

---
+++ Задача 50/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите номер строки");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int st=Convert.ToInt32(Console.ReadLine());
+int n = 4;
+int m = 4;
+int [,] numbers  = new int [n, m];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+if(num<numbers.GetLength(0)&& st<numbers.GetLength(0))
+{
+    Console.WriteLine(numbers[num,st]);
+}
+else Console.WriteLine("Такого числа  массиве нет");
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10) ;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
---
+++ Задача 52/Задача 52.cs
@@ -0,0 +1,24 @@
+﻿Random random  = new Random ();
+double  sum=0;
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j ++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("---------------------------");
+
+for (int j = 0; j < arr.GetLength(1); j ++)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum  += arr [i, j];
+    }
+    double average = sum / arr.GetLength(0);
+    int n = j + 1;
+    Console.WriteLine("{0: 0.000}"+ average);
+}
